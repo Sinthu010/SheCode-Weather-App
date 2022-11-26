6f5c21bcb5b0eafb71ca3de12a8100ad985eb411
Sinthu010 <sinthuja010@gmail.com>
2022-11-26 16:38:23 +0000

    add forecast
    add function to loop through the HTML for each day and add column in the row

--- weather-app-top/src/weather-app-top.js
+++ weather-app-top/src/weather-app-top.js
@@ -55,6 +55,28 @@ function convertToC(event) {
   tempElement.innerHTML = Math.round(celciusTemperature);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="50"/>
+      <div class="weather-forecast-temps">
+        <span class="temp-max"> 18 </span>
+        <span class="temp-min"> 12 </span>
+      </div>
+    </div>
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let celciusTemperature = null;
 
 let now = new Date();
@@ -97,3 +119,5 @@ fButton.addEventListener("click", convertToF);
 
 let cButton = document.querySelector("#celc");
 cButton.addEventListener("click", convertToC);
+
+displayForecast();
