4b9bbe9206ecb9bb7c249fd5d7cf10adca19441b
Sinthu010 <sinthuja010@gmail.com>
2022-11-27 23:45:58 +0000

    change file names

---
+++ weather-app-top/final/app.js
@@ -0,0 +1,140 @@
+function getForecast(coordinates) {
+  let apiKey = "2abt2f6340do47141b6f92d3a14301be";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}`;
+  axios.get(apiURL).then(displayForecast);
+}
+
+function getTemp(response) {
+  celciusTemperature = response.data.main.temp;
+  let tempRound = Math.round(celciusTemperature);
+  let humidityRound = Math.round(response.data.main.humidity);
+  let windRound = Math.round(response.data.wind.speed);
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = tempRound;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = humidityRound;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = windRound;
+  let description = response.data.weather[0].description;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  getForecast(response.data.coord);
+}
+
+function changeCityName(event) {
+  event.preventDefault();
+  let userCityForm = document.querySelector("#city-name");
+  let cityElement = document.querySelector("#city");
+  let cityName = userCityForm.value;
+  cityElement.innerHTML = cityName;
+  let apiKey = "f8e6a9e3d6fde87cb38868da460b1371";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(getTemp);
+}
+
+function currentPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "f8e6a9e3d6fde87cb38868da460b1371";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(getTemp);
+}
+
+function convertToF(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temp");
+  cButton.classList.remove("active");
+  fButton.classList.add("active");
+  let tempF = (celciusTemperature * 9) / 5 + 32;
+  tempElement.innerHTML = Math.round(tempF);
+}
+
+function convertToC(event) {
+  event.preventDefault();
+  cButton.classList.add("active");
+  fButton.classList.remove("active");
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = Math.round(celciusTemperature);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  x = 0;
+  let date = new Date();
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  forecast.forEach(function (forecastDay) {
+    tempMax = Math.round(response.data.daily[x].temperature.maximum);
+    tempMin = Math.round(response.data.daily[x].temperature.minimum);
+    iconURL = response.data.daily[x].condition.icon_url;
+
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-sm">
+      <div class="weather-forecast-date">${days[day]}</div>
+      <img src=${iconURL} alt="" width="50"/>
+      <div class="weather-forecast-temps">
+        <span class="temp-max"> ${tempMax}°C </span>
+        <span class="temp-min"> ${tempMin}°C </span>
+      </div>
+    </div>
+
+  `;
+    x = x + 1;
+    day = day + 1;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+let celciusTemperature = null;
+
+let now = new Date();
+
+let day = now.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+day = days[now.getDay()];
+
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let mins = now.getMinutes();
+if (mins < 10) {
+  mins = `0${mins}`;
+}
+
+let dateTime = document.querySelector("#date-time");
+dateTime.innerHTML = `${day} ${hour}:${mins}`;
+
+let searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", changeCityName);
+
+let locationButton = document.querySelector(".current-location-button");
+locationButton.addEventListener(
+  "click",
+  navigator.geolocation.getCurrentPosition(currentPosition)
+);
+
+let fButton = document.querySelector("#faren");
+fButton.addEventListener("click", convertToF);
+
+let cButton = document.querySelector("#celc");
+cButton.addEventListener("click", convertToC);
